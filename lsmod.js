906ae1653ab4ea4921b995d7e46d5383d5d82393
DarkMark1991 <amiran.ramishvili@gmail.com>
2017-01-05 14:06:14 +0400

    06 MAKE IT MODULAR

--- lsmod.js
+++ lsmod.js
@@ -4,12 +4,12 @@ module.exports = function(path, prefix, callback){
     prefix = "." + prefix;
 
     fs.readdir(path, function (err, files){
-	if(err) return callback(err);
-	var j = 0;
-	var data = new Array();
-	for(var i=0; i<files.length; i++)
-    	    if(files[i].substr("-" + prefix.length) == prefix)
+        if(err) return callback(err);
+        var j = 0;
+        var data = new Array();
+        for(var i=0; i<files.length; i++)
+            if(files[i].substr("-" + prefix.length) == prefix)
                 data.push(files[i]);
-	return callback(null, data);
+        return callback(null, data);
     });
 }
